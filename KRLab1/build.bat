244161c10837855caa1471710cd2b51f4221b7e1
Mikhail <mih-med@hotmail.com>
2018-10-30 23:19:53 +0300

    終わった。

--- KRLab1/build.bat
+++ KRLab1/build.bat
@@ -12,11 +12,11 @@ set LIBDIR_WK=%CompillersDir%\C++\Windows Kits\10\Lib\10.0.16299.0\um\x86
 
 set LIBS="libcpmt.lib" "libcmt.lib" "oldnames.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" "msvcrtd.lib" "libucrt.lib"
 
-set SOURCES="%ProjectDir%main.cpp" "%ProjectDir%readData.cpp"
-set SOURCES_Compiler="%ProjectDir%main.cpp" "%ProjectDir%readData.cpp"
+set SOURCES="%ProjectDir%main.cpp" "%ProjectDir%dataProcessing.cpp"
+set SOURCES_Compiler="%ProjectDir%main.cpp" "%ProjectDir%dataProcessing.cpp"
 
-set OBJECTS="%ProjectDir%main.obj" "%ProjectDir%readData.obj"
-set OBJECTS_Compiler=/Fo"%ProjectDir%main.obj" "%ProjectDir%readData.obj"
+set OBJECTS="%ProjectDir%main.obj" "%ProjectDir%dataProcessing.obj"
+set OBJECTS_Compiler=/Fo"%ProjectDir%main.obj" "%ProjectDir%dataProcessing.obj"
 
 set CompilerC++=%CompillersDir%\C++\MSVC\14.13.26128\bin\Hostx86\x86
 
@@ -28,6 +28,6 @@ SET LIB=%LIBDIR_MSVC%;%LIBDIR_WK%;%CompillersDir%\C++\Windows Kits\10\Lib\10.0.1
 
 cd %ProjectDir%
 %CompilerC++%\cl.exe /Fo"main.obj" /c "main.cpp" /FAs /EHsc /Od /Ob1 /Oi /Oy- /Z7 /I "%ProjectDir%"
-%CompilerC++%\cl.exe /Fo"readData.obj" /c "readData.cpp" /FAs /EHsc /Od /Ob1 /Oi /Oy- /Z7 /I "%ProjectDir%"
+%CompilerC++%\cl.exe /Fo"dataProcessing.obj" /c "dataProcessing.cpp" /FAs /EHsc /Od /Ob1 /Oi /Oy- /Z7 /I "%ProjectDir%"
 %CompilerC++%\link.exe %OBJECTS% /OUT:"%ProjectDir%Saek0.exe" /DEBUG /MACHINE:"X86" /INCREMENTAL:NO /MAP /SUBSYSTEM:"CONSOLE" /NOLOGO
 pause
